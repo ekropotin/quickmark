dfaa692abc0f6432fddb2c2498bf5ce6ddfa4eb2
Evgeny Kropotin <ekropotin@gmail.com>
2025-09-05 13:33:07 -0700

    ci: fix release-server workflow by specifying fetch dept and fetch tags flag for checkout. Update brew formula (#143)
    - fix release-server workflow by specifying fetch dept and fetch tags flag for checkout
    - Update brew formula

--- pkg/homebrew/Formula/quickmark-cli.rb
+++ pkg/homebrew/Formula/quickmark-cli.rb
@@ -2,23 +2,15 @@ class QuickmarkCli < Formula
   desc "Lightning-fast Markdown/CommonMark linter CLI tool with tree-sitter based parsing"
   homepage "https://github.com/ekropotin/quickmark"
   license "MIT"
-  version "1.0.0"
+  version "1.1.0"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/ekropotin/quickmark/releases/download/quickmark-cli%40#{version}/qmark-x86_64-apple-darwin"
-      sha256 "309161921d26ea93f1b8f3f6738346bcf032e42a12b600363b43f76f87158bba"
+      url "https://github.com/ekropotin/quickmark/releases/download/quickmark-cli%40#{version}/qmark-x86_64-apple-darwin.tar.gz"
+      sha256 "sha256:90740f9c0632d8b1da4d00c9c6361c01eb9a72c074641f6952723e6583bbdd8d"
     else
-      url "https://github.com/ekropotin/quickmark/releases/download/quickmark-cli%40#{version}/qmark-aarch64-apple-darwin"
-      sha256 "c6cc057df011d1df9ee2d0a60d6f2634d78561b57d3afd85cbd89715d737649d"
-    end
-  end
-
-  def install
-    if Hardware::CPU.intel?
-      bin.install "qmark-x86_64-apple-darwin" => "qmark"
-    else
-      bin.install "qmark-aarch64-apple-darwin" => "qmark"
+      url "https://github.com/ekropotin/quickmark/releases/download/quickmark-cli%40#{version}/qmark-aarch64-apple-darwin.tar.gz"
+      sha256 "sha256:4466f54fd304d34d21dba7871a09d4d24df6c23f7cee48fae1f4a6a1f5466855"
     end
   end
 
